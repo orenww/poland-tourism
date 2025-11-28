be4f680188d4d75c02b9dbb8fc285dd8463883c4
אורן וינברג <weinberg.oren@gmail.com>
2025-11-28 16:06:04 +0200

    Add rate limiting with throttler guard

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,28 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ItemsModule } from './items/items.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [PrismaModule, ItemsModule, AuthModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // 60 seconds
+        limit: 10, // 10 requests per 60 seconds
+      },
+    ]),
+    PrismaModule,
+    ItemsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
